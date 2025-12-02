1e05b09e2e706a86edafab36a2fc3f7936978493
Narendrakumar-Suresh <narendrkumarsuresh@gmail.com>
2025-12-02 16:19:23 +0000

    day 2 babe!

---
+++ day2/q2.py
@@ -0,0 +1,21 @@
+import re
+
+f=open("./day2/input.txt","r")
+lis=f.read().split(',')
+invalid=[]
+
+def is_invalid(i):
+    s=str(i)
+    return re.fullmatch(r"(\d+)\1+", s) is not None
+
+for i in range(len(lis)):
+    start,end=lis[i].split('-')
+    start,end=int(start),int(end)
+
+    for i in range(start,end+1):
+        if is_invalid(i):
+            invalid.append(i)
+
+print(sum(invalid))
+
+
